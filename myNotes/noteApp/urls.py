cbc437f73bf9d366ca31bfb735945174b48dd4fc
KonstantinaK98 <konakara98@gmail.com>
2021-02-09 00:03:14 +0200

    Changes for favicon.ico second try

--- myNotes/noteApp/urls.py
+++ myNotes/noteApp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.conf.urls import url
 from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', views.notes, name="notes"),
+    path('/', views.notes, name="notes"),
     path('settings/', views.settings, name="settings"),
     path('notes/', views.notes, name="notes"),
     path('editNote/<str:pk_note>/', views.editNote, name="editNote"),
@@ -13,4 +16,5 @@ urlpatterns = [
     path('register/', views.registerPage, name="registerPage"),
     path('logout/', views.logoutPage, name="logoutPage"),
     url('change_password/', views.change_password, name='change_password'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
 ]
